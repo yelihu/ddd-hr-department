0ae66e52f6f73ac222cadec1b92173847289a1c2
叶李虎 <860536662@qq.com>
2023-05-21 12:00:37 +0800

    feat(maven): 项目初始化

---
+++ README.md
@@ -0,0 +1,1 @@
+# ddd-hr-department
---
+++ department-domain/src/main/java/org/dddexample/hr/App.java
@@ -0,0 +1,13 @@
+package org.dddexample.hr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
